bdef8df1014e6e1fa22588c6da75c5515792206f
Owner <Owner@192.168.2.101>
2020-07-16 09:05:53 +0900

    aa

--- lesson/src/kosyu/Toi05.java
+++ lesson/src/kosyu/Toi05.java
@@ -1,10 +1,12 @@
 package kosyu;
 
+import java.util.Scanner;
+
 public class Toi05 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		Scanner scan = new Scanner(System.in);
 	}
 
 }
